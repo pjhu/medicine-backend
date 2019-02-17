5a3075338ead033bba2812bd1c19ab608358333e
pjhu <pengjinhu26@gmail.com>
2019-02-17 16:55:29 +0800

    export orders to csv file

---
+++ src/main/java/com/pjhu/medicine/application/endpoint/ReportController.java
@@ -0,0 +1,30 @@
+package com.pjhu.medicine.application.endpoint;
+
+import com.pjhu.medicine.application.ResponseCreator;
+import com.pjhu.medicine.domain.report.ReportAssembler;
+import com.pjhu.medicine.domain.report.order.Order;
+import com.pjhu.medicine.domain.report.order.OrderReporter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/reports", method = RequestMethod.GET)
+@RequiredArgsConstructor
+public class ReportController {
+
+    private final OrderReporter orderReporter;
+    private final ReportAssembler reportAssembler;
+
+    @GetMapping(value = "/orders")
+    public ResponseEntity<byte[]> orders() {
+        List<Order> allOrders = orderReporter.findAllOrders();
+        byte[] data = reportAssembler.assemble(allOrders, Order.class);
+        return ResponseCreator.createAttachmentResponse("order.csv", data);
+    }
+}
---
+++ src/main/java/com/pjhu/medicine/domain/report/ReportAssembler.java
@@ -0,0 +1,30 @@
+package com.pjhu.medicine.domain.report;
+
+import com.fasterxml.jackson.dataformat.csv.CsvGenerator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ReportAssembler {
+
+    private CsvMapper csvMapper = new CsvMapper();
+
+    public ReportAssembler() {
+        csvMapper.registerModule(new JavaTimeModule());
+    }
+
+    @SneakyThrows
+    public byte[] assemble(List<?> items, Class<?> type) {
+        if (null == items) {
+            return new byte[0];
+        }
+        CsvSchema schema = csvMapper.schemaFor(type).withHeader();
+        csvMapper.configure(CsvGenerator.Feature.ALWAYS_QUOTE_STRINGS, true);
+        return csvMapper.writer(schema).writeValueAsBytes(items);
+    }
+}
